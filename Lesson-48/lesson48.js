e22a2b96e2c51e6fbf3f2f4928b2b521171695e2
Asraful Islam <iasraful321@gmail.com>
2021-08-28 21:20:08 +0600

    added 48 lesson and done with practice

---
+++ Lesson-48/lesson48.js
@@ -0,0 +1,20 @@
+const num = 0
+
+switch (num) {
+    case 0:
+        console.log("it is Zero");
+        break;
+    case 1 : 
+    case 2 : 
+    console.log("It is small");
+    break
+    case 3:
+        case 4:
+            console.log("it is Medium");
+            break
+            case 5 : 
+            console.log("THis is a large Number");
+    default:
+        console.log("This is none of those print 'try again'");
+        break;
+}
