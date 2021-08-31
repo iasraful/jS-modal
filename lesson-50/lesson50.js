fb58f758844d73bd7f4313bcca49a74af2ac3ac3
Asraful Islam <iasraful321@gmail.com>
2021-08-31 23:26:04 +0600

    updated

--- lesson-50/lesson50.js
+++ lesson-50/lesson50.js
@@ -1,5 +1,22 @@
-let i = 0
-while (i < 9 ) {
-    console.log(i);
-    i++
+// let i = 0
+// while (i < 9 ) {
+//     console.log(i);
+//     i++
+// }
+
+const person = {
+    name: "Asraful",
+    friend: {
+        name: "neha",
+        friend: {
+            name: "myBB"
+        } 
+    }
+}
+
+let newPerson = person
+
+while (person.name != null) {
+    console.log(name);
+    newPerson = person.name
 }
